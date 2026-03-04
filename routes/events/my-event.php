4bf5edf8916a051a0734ed3bad33a693fc3a43bc
Watcharit Jaroenchai <124548035+Fokkio@users.noreply.github.com>
2026-03-04 11:49:28 +0700

    Delete routes directory

--- routes/events/my-event.php
+++
@@ -1,5 +0,0 @@
-    <?php
-        $userId = $_SESSION['user_id'];
-        $events = getEventsByCreatorId($userId);
-        creatorcheck($events[0]['creator_id'], '/events');
-        renderView('my-events', ['title' => 'My Events', 'events' => $events]);
--- routes/users/profile.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-$userId = $_SESSION['user_id'];
-$userProfile = getUserProfileDetails($userId);
-renderView('profile', ['title' => 'My Profile', 'user' => $userProfile]);
