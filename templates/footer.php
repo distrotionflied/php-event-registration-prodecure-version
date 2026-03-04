1519c02b2984458ab7cd50ddf0b813bfee8cfd5a
Watcharit Jaroenchai <124548035+Fokkio@users.noreply.github.com>
2026-03-04 11:49:35 +0700

    Delete templates directory

--- templates/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- Header และ Footer อาจแยกออกเป็นไฟล์แยกต่างหากได้ -->
-<footer class="bg-white border-t border-gray-200 py-6 w-full mt-auto">
-<p class="text-center text-sm text-gray-500 font-medium">
-&copy; <?= date('Y') ?>. All rights reserved by Natapat Trajan.
-</p>
-</footer>
-<!-- Header และ Footer อาจแยกออกเป็นไฟล์แยกต่างหากได้ -->
